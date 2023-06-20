c5c5b8e698d42a94b048634f40b6bd47fc027cff
ttopde_nike <tsering.topden@nike.com>
2023-06-20 17:43:49 +0800

    feat: added two new attribute

--- src/main/java/org/nike/bot/webb/DataRecord.java
+++ src/main/java/org/nike/bot/webb/DataRecord.java
@@ -12,6 +12,8 @@ public class DataRecord {
     private String value;
     private String author;
     private String ttl;
+    private String action;
+    private String nameSpace;
     @JsonIgnore
     private List<Map<String, List<String>>> metadata;
 
@@ -64,6 +66,22 @@ public class DataRecord {
         this.ttl = ttl;
     }
 
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getNameSpace() {
+        return nameSpace;
+    }
+
+    public void setNameSpace(String nameSpace) {
+        this.nameSpace = nameSpace;
+    }
+
     public List<Map<String, List<String>>> getMetadata() {
         return metadata;
     }
@@ -79,6 +97,8 @@ public class DataRecord {
                 ", \"value\":\"" + value + '\"' +
                 ", \"author\":\"" + author + '\"' +
                 ", \"ttl\":\"" + ttl + '\"' +
+                ", \"action\":\"" + action + '\"' +
+                ", \"nameSpace\":\"" + nameSpace + '\"' +
                 '}';
     }
 
@@ -93,6 +113,8 @@ public class DataRecord {
         if (value != null ? !value.equals(that.value) : that.value != null) return false;
         if (author != null ? !author.equals(that.author) : that.author != null) return false;
         if (ttl != null ? !ttl.equals(that.ttl) : that.ttl != null) return false;
+        if (action != null ? !action.equals(that.action) : that.action != null) return false;
+        if (nameSpace != null ? !nameSpace.equals(that.nameSpace) : that.nameSpace != null) return false;
         return metadata != null ? metadata.equals(that.metadata) : that.metadata == null;
     }
 
@@ -102,6 +124,8 @@ public class DataRecord {
         result = 31 * result + (value != null ? value.hashCode() : 0);
         result = 31 * result + (author != null ? author.hashCode() : 0);
         result = 31 * result + (ttl != null ? ttl.hashCode() : 0);
+        result = 31 * result + (action != null ? action.hashCode() : 0);
+        result = 31 * result + (nameSpace != null ? nameSpace.hashCode() : 0);
         result = 31 * result + (metadata != null ? metadata.hashCode() : 0);
         return result;
     }
